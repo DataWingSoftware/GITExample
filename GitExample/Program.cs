13491c068d6891294ce4934210f0284935813683
Mark Walls <markw@datawingsoftware.com>
2016-07-26 08:53:11 -0700

    This is where we are going to start the presentation

---
+++ GitExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private int TestingGIT()
+        {
+            return 1;
+        }
+    }
+}
